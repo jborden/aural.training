769ed501d7005fdd2f46ddd89dbc34c8f01c1e8d
James Borden <jmborden@gmail.com>
2022-09-13 00:03:29 -0400

    start / stop button working ; beginning of event message log

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,25 @@
-import { freelizer } from 'freelizer'
+import { freelizer } from 'freelizer';
 
-;(async function () {
-    try {
-      const { start, subscribe } = await freelizer()
-      start()
-      subscribe(console.log)
-    } catch (error) {
-      // Error handling goes here
+let start: Function, stop: Function, subscribe: Function;
+export async function startAudio() {
+  try {
+    if (typeof start == "undefined") {
+      ({start, stop, subscribe} = await freelizer());
     }
-})()
+    start();
+    subscribe(console.log);
+  } catch (error){
+    console.log("There was an error trying to start the audio");
+    console.log(error);
+  }
+}
 
+export async function stopAudio() {
+  try {
+    console.log(stop);
+    stop()
+  } catch (error){
+    console.log("There was an error trying to stop the audio");
+    console.log(error);
+  }
+}
