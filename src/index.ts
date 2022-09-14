330e67dff214e248ada797aa44d6a465383e2e7a
James Borden <jmborden@gmail.com>
2022-09-13 23:25:19 -0400

    log frequency and note in dom

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,18 @@
 import { freelizer } from 'freelizer';
 
 let start: Function, stop: Function, subscribe: Function;
+function publishEvents(e: any) {
+  const event = new CustomEvent("audioSignal",{detail: e});
+  dispatchEvent(event);
+}
+
 export async function startAudio() {
   try {
     if (typeof start == "undefined") {
       ({start, stop, subscribe} = await freelizer());
     }
     start();
-    subscribe(console.log);
+    subscribe(publishEvents);
   } catch (error){
     console.log("There was an error trying to start the audio");
     console.log(error);
@@ -23,3 +28,12 @@ export async function stopAudio() {
     console.log(error);
   }
 }
+
+export function logListener(e: any) {
+  let {frequency, note, noteFrequency, deviation, octave} = e.detail;
+  let audioEventLogElem = document.querySelector("#audio-event-log");
+  audioEventLogElem.innerHTML = `<p>frequency = ${frequency.toFixed(2)} note = ${note} octave=${octave}<p>`;
+
+}
+
+addEventListener('audioSignal',logListener);
