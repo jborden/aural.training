db3bae84abdcf61789acc242afbcbd0d2cf1a286
James Borden <jmborden@gmail.com>
2022-09-30 21:35:44 -0400

    choose different tunings and strings

--- src/index.ts
+++ src/index.ts
@@ -7,5 +7,21 @@ exports = {startAudio, stopAudio}
 // listener for the bottom
 addEventListener('audioSignal',logListener);
 // add guitar
-guitarSvg(document.querySelector("#guitar"))
-guitarController();
+// drop-D tuning
+const dropDTuning = [{note: "E", octave: 4},
+		     {note: "B", octave: 3},
+		     {note: "G", octave: 3},
+		     {note: "D", octave: 3},
+		     {note: "A", octave: 2},
+		     {note: "E", octave: 2}]
+// https://en.wikipedia.org/wiki/Seven-string_guitar#Tuning
+const standard7String = [{note: "E", octave: 4},
+			 {note: "B", octave: 3},
+			 {note: "G", octave: 3},
+			 {note: "D", octave: 3},
+			 {note: "A", octave: 2},
+			 {note: "E", octave: 2},
+			 {note: "B", octave:1}]
+//guitarSvg(document.querySelector("#guitar"),7,12,standard7String);
+guitarSvg(document.querySelector("#guitar"),6,12);
+//guitarController();
