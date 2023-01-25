0161baeec47ae9fdb843738fda2c903f81131685
James Borden <jmborden@gmail.com>
2023-01-24 23:11:03 -0500

    create rollup.config.compile.cs for production compilation

--- rollup.config.compile.js
+++ rollup.config.compile.js
@@ -1,15 +1,31 @@
 import typescript from '@rollup/plugin-typescript';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
-
+import livereload from 'rollup-plugin-livereload';
+import commonjs from '@rollup/plugin-commonjs';
+import babel from '@rollup/plugin-babel';
+import replace from '@rollup/plugin-replace';
 
 export default {
-  input: 'src/ts/index.ts',
+  input: 'src/ts/index.tsx',
   output: {
     file: 'resources/public/js/bundle.js',
     format: 'iife',
-    sourcemap: 'inline'
+    sourcemap: 'inline',
+    inlineDynamicImports: true,
+    globals: {
+      "react/jsx-runtime":"jsxRuntime",
+      "react-dom/client":"ReactDOM",
+      "react":"React"},
   },
-  watch: false,
+  watch: true,
   plugins: [typescript(),
-	    nodeResolve()]
-};
+            nodeResolve(),
+            livereload(),
+            // this should be 'production' for minified code
+            replace({preventAssignment: true,
+		     'process.env.NODE_ENV': JSON.stringify('production')}),
+            commonjs(),
+            babel({
+            babelHelpers: "bundled",
+               exclude: 'node_modules/**',
+               presets: [["@babel/preset-react", {"runtime": "automatic"}]]})]};
